a93fdd4b2304a9305c1f17e0f9471a374a6866a1
Carlos Eduardo Soeiro Monteiro <94386820+cadu-monteiro@users.noreply.github.com>
2024-10-13 05:41:24 +0000

    Add files via upload

---
+++ assets/Js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const idColuna = document.getElementById('idColuna');
+        idColuna.classList.toggle('coluna');
+    
+    const idColuna2 = document.getElementById('idColuna2');
+        idColuna2.classList.toggle('coluna2');    
+}
