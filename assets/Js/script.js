97c7146fd0908e22f9fb1a1d98110add6a27efbc
Carlos Eduardo Soeiro Monteiro <94386820+cadu-monteiro@users.noreply.github.com>
2024-10-13 18:39:20 +0000

    Delete assets directory

--- assets/Js/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const idColuna = document.getElementById('idColuna');
-        idColuna.classList.toggle('coluna');
-    
-    const idColuna2 = document.getElementById('idColuna2');
-        idColuna2.classList.toggle('coluna2');    
-}
